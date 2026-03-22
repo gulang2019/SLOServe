515e83c089d0a61483d3a6f8e18522e7ac76af2b
gulang2019 <siyuanc3@andrew.cmu.edu>
2026-03-22 17:47:37 -0400

    add qwen3_moe config

--- SLOsServe/model_config.py
+++ SLOsServe/model_config.py
@@ -21,8 +21,9 @@ class ModelConfig:
 def get_model_config(
     model_tag: str 
 ) -> ModelConfig:
-    from transformers import AutoConfig, OPTConfig, GPT2Config, Qwen2Config
+    from transformers import AutoConfig, OPTConfig, GPT2Config, Qwen2Config, Qwen3MoeConfig
     config = AutoConfig.from_pretrained(model_tag)
+    print('Loaded', type(config))
     if isinstance(config, GPT2Config):
         intermidiate_size = config.n_inner if config.n_inner is not None else 4 * config.n_embd
         # print('config', config)
@@ -94,6 +95,59 @@ def get_model_config(
             n_param = config.num_hidden_layers * (config.hidden_size ** 2 * 4 + config.hidden_size * config.intermediate_size * 2)\
             + config.hidden_size * config.vocab_size,
             max_seq_len = 16384,# config.max_position_embeddings,
+            intermidiate_size = config.intermediate_size
+        )
+    elif isinstance(config, Qwen3MoeConfig):
+        """
+        Qwen3MoeConfig {
+        "architectures": [
+            "Qwen3MoeForCausalLM"
+        ],
+        "attention_bias": false,
+        "attention_dropout": 0.0,
+        "bos_token_id": 151643,
+        "decoder_sparse_step": 1,
+        "dtype": "bfloat16",
+        "eos_token_id": 151645,
+        "head_dim": 128,
+        "hidden_act": "silu",
+        "hidden_size": 2048,
+        "initializer_range": 0.02,
+        "intermediate_size": 6144,
+        "max_position_embeddings": 40960,
+        "max_window_layers": 48,
+        "mlp_only_layers": [],
+        "model_type": "qwen3_moe",
+        "moe_intermediate_size": 768,
+        "norm_topk_prob": true,
+        "num_attention_heads": 32,
+        "num_experts": 128,
+        "num_experts_per_tok": 8,
+        "num_hidden_layers": 48,
+        "num_key_value_heads": 4,
+        "output_router_logits": false,
+        "rms_norm_eps": 1e-06,
+        "rope_scaling": null,
+        "rope_theta": 1000000.0,
+        "router_aux_loss_coef": 0.001,
+        "sliding_window": null,
+        "tie_word_embeddings": false,
+        "transformers_version": "4.57.1",
+        "use_cache": true,
+        "use_sliding_window": false,
+        "vocab_size": 151936
+        }
+        """
+        return ModelConfig(
+            tag = model_tag,
+            embed_size = config.hidden_size,
+            num_heads = config.num_key_value_heads, 
+            head_dim = config.hidden_size // config.num_attention_heads,
+            vocab_size = config.vocab_size,
+            n_layer = config.num_hidden_layers,
+            n_param = config.num_hidden_layers * (config.hidden_size ** 2 * 4 + config.hidden_size * config.intermediate_size * 2)\
+            + config.hidden_size * config.vocab_size,
+            max_seq_len = config.max_position_embeddings,
             intermidiate_size = config.intermediate_size
         )
     elif 'deepseek' in model_tag:
